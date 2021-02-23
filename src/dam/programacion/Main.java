2732e9032a9d0b312db84a02955ff15e4cf77770
Jose Angel Sampere <jsamperevazquez@danielcastelao.org>
2021-02-23 18:16:22 +0100

    Añadido los objetos

--- src/dam/programacion/Main.java
+++ src/dam/programacion/Main.java
@@ -1,8 +1,15 @@
 package dam.programacion;
 
+import libreriaCOD.Consola;
+import libreriaCOD.MostrarMensajes;
+import libreriaCOD.Ventana;
+
 public class Main {
 
     public static void main(String[] args) {
-
+        Ventana objetoVentana = new Ventana();
+        objetoVentana.MostrarErrorJoptionPane("Deseas continuar");
+        Consola objetoConsola = new Consola();
+        objetoConsola.mostrarMensajeSout("Mensaje por consola");
     }
 }
