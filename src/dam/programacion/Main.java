2f75a9271f452acc2397f6b6d76932bbef853080
Jose Angel Sampere <jsamperevazquez@danielcastelao.org>
2021-03-02 11:41:55 +0100

    Modificado main para nuevas factorías

--- src/dam/programacion/Main.java
+++ src/dam/programacion/Main.java
@@ -1,15 +1,28 @@
 package dam.programacion;
 
-import libreriaCOD.Consola;
-import libreriaCOD.MostrarMensajes;
-import libreriaCOD.Ventana;
+import libreriaCOD.*;
 
 public class Main {
 
     public static void main(String[] args) {
-        Ventana objetoVentana = new Ventana();
-        objetoVentana.MostrarErrorJoptionPane("Deseas continuar");
-        Consola objetoConsola = new Consola();
-        objetoConsola.mostrarMensajeSout("Mensaje por consola");
+      /*
+       Consola objetoConsloa = new Consola();
+       objetoConsloa.visualizar("Hola");
+       Ventana objetoVentana = new Ventana();
+       objetoVentana.visualizar("Adios");
+    */
+        InterfaceMetodosComunes miSalida,misalida2,misalida3,miEntrada1,miEntrada2;
+        miSalida=FactoriaDeSalidas.getProductoSalida(FactoriaDeSalidas.CONSOLA);
+        miSalida.visualizar("Hola");
+        misalida2=FactoriaDeSalidas.getProductoSalida(FactoriaDeSalidas.VENTANA);
+        misalida2.visualizar("Adios");
+        misalida3=FactoriaDeSalidas.getProductoSalida(FactoriaDeSalidas.IMPRESORA);
+        misalida3.visualizar("Imprimiendo por impresora");
+        miEntrada1= FactoriaDeEntradas.getProductoEntrada(FactoriaDeEntradas.CONSOLA);
+        miEntrada1.introducirDatos();
+        miEntrada2=FactoriaDeEntradas.getProductoEntrada(FactoriaDeEntradas.VENTANA);
+        miEntrada2.introducirDatos();
+
     }
+
 }
